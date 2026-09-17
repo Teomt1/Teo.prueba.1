4fc2442e2678fdf746c6c2addc673b5a8b76e1c8
Teomt1 <121972688+Teomt1@users.noreply.github.com>
2026-09-17 20:19:11 -0300

    Medio de la clase, cambios de un readonly y un par de botones y checklists por mi cuenta (sin funcionalidad aún)

---
+++ PedidosMaterial/SolicitudMaterialForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Teo.prueba._1.PedidosMaterial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            botonsalir = new Button();
+            checkedListBox1 = new CheckedListBox();
+            SuspendLayout();
+            // 
+            // botonsalir
+            // 
+            botonsalir.Location = new Point(333, 321);
+            botonsalir.Name = "botonsalir";
+            botonsalir.Size = new Size(88, 26);
+            botonsalir.TabIndex = 0;
+            botonsalir.Text = "Salir";
+            botonsalir.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Items.AddRange(new object[] { "Opcion 1", "Opcion 2", "Opcion 3", "Opcion 4", "Opcion 5" });
+            checkedListBox1.Location = new Point(295, 90);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(152, 130);
+            checkedListBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(checkedListBox1);
+            Controls.Add(botonsalir);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button botonsalir;
+        private CheckedListBox checkedListBox1;
+    }
+}
---
+++ PedidosMaterial/SolicitudMaterialForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Teo.prueba._1.PedidosMaterial
+{
+    public partial class Form1 : Form
+    {
+        private readonly Form1 modelo = new();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
